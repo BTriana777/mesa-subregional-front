346fbdaa3f68251022eb0e53e03d4ff621ef2ec6
Brandon Triana <trianab659@gmail.com>
2022-03-30 07:47:03 -0500

    nav now has a better animation, i will start with responsive mode

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -24,50 +24,31 @@ export const NavBar = () => {
       <div className='nav-bnt-img-ul-container'>
         <div className='btn-nav-container' onClick={handleClickOpenNav}>
           {
-            navOpen?
-            <>
-              <IoMdClose color='#fff' size={'35px'} />
-              <p>MSJO</p>
-            </>
-            :
-              <IoMdMenu color='#fff' size={'35px'} />
+          navOpen?
+            <IoMdClose color='#fff' size={'35px'} />
+          :
+            <IoMdMenu color='#fff' size={'35px'} />
           }
+          <p>MSJO</p>
         </div>
         <img src="https://i.imgur.com/Bs8H3EA.png" alt="mesa-logo" className='img-nav' />
         <ul>
             <li>
-              {
-                navOpen?
-                  <div className='icon-text-container'><FaHome className='icon-nav' size={'38px'} cursor='pointer'/><span>Home</span></div>
-                :
-                  <FaHome className='icon-nav' size={'38px'} cursor='pointer'/>
-              }
+              <div className='icon-text-container'><FaHome className='icon-nav' size={'38px'} cursor='pointer'/><span>Home</span></div>
             </li>
             <li>
-              {
-                navOpen?
-                  <div className='icon-text-container'><FaInfo className='icon-nav' size={'36px'} cursor='pointer'/><span>Historia</span></div>
-                :
-                  <FaInfo className='icon-nav' size={'36px'} cursor='pointer'/>
-              }
+
+              <div className='icon-text-container'><FaInfo className='icon-nav' size={'36px'} cursor='pointer'/><span>Historia</span></div>
+
             </li>
             <li>
-              {
-                navOpen?
-                  <div className='icon-text-container'><BiNews className='icon-nav' size={'38px'} cursor='pointer'/><span>Noticias</span></div>
-                :
-                  <BiNews className='icon-nav' size={'38px'} cursor='pointer'/>
-              }
+              <div className='icon-text-container'><BiNews className='icon-nav' size={'38px'} cursor='pointer'/><span>Noticias</span></div>
             </li>
         </ul>
       </div>
       <div className='nav-user-container'>
-        {
-          navOpen?
-            <div className='icon-text-container'><span>Brandon</span><FaUserAlt className='icon-nav' size={'38px'} cursor='pointer' /></div>
-          :
-            <FaUserAlt className='icon-nav' size={'38px'} cursor='pointer' />
-        }
+        <span>Brandon</span>
+        <FaUserAlt className='icon-nav' size={'38px'} cursor='pointer' />
       </div>
     </nav>
   )
