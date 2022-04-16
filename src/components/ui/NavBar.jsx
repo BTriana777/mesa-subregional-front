e300cafa9fed54fc97ac7e958f6261c2106b0a6b
Brandon Triana <trianab659@gmail.com>
2022-04-16 16:54:14 -0500

    put navigate funtion into the navbar

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,58 +1,85 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { uiCloseNav, uiOpenNav } from '../../actions/ui';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { uiCloseNav, uiOpenNav } from "../../actions/ui";
 
-import { FaHome, FaInfo, FaUserAlt} from 'react-icons/fa';
-import { BiNews } from 'react-icons/bi';
-import {IoMdMenu, IoMdClose, IoMdPhotos } from 'react-icons/io';
+import { FaHome, FaInfo, FaUserAlt } from "react-icons/fa";
+import { BiNews } from "react-icons/bi";
+import { IoMdMenu, IoMdClose, IoMdPhotos } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
-
-  const { navOpen } = useSelector(state => state.ui)
+  const { navOpen } = useSelector((state) => state.ui);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleClickOpenNav = () => {
-    if(navOpen){
-      dispatch( uiCloseNav() );
+    if (navOpen) {
+      dispatch(uiCloseNav());
     } else {
-      dispatch( uiOpenNav() );
+      dispatch(uiOpenNav());
     }
-  }
+  };
 
   return (
-    <nav className={`${navOpen? 'nav-main-container active' : 'nav-main-container'}`}>
-      <div className='nav-bnt-img-ul-container'>
-        <div className='btn-nav-container' onClick={handleClickOpenNav}>
-          {
-          navOpen?
-            <IoMdClose color='#fff' size={'35px'} cursor='pointer' />
-          :
-            <IoMdMenu color='#fff' size={'35px'} cursor='pointer' />
-          }
+    <nav
+      className={`${
+        navOpen ? "nav-main-container active" : "nav-main-container"
+      }`}
+    >
+      <div className="nav-bnt-img-ul-container">
+        <div className="btn-nav-container" onClick={handleClickOpenNav}>
+          {navOpen ? (
+            <IoMdClose color="#fff" size={"35px"} cursor="pointer" />
+          ) : (
+            <IoMdMenu color="#fff" size={"35px"} cursor="pointer" />
+          )}
           <p>MSJO</p>
         </div>
-        <img src="https://i.imgur.com/Bs8H3EA.png" alt="mesa-logo" className='img-nav' />
+        <img
+          src="https://i.imgur.com/Bs8H3EA.png"
+          alt="mesa-logo"
+          className="img-nav"
+        />
         <ul>
-            <li>
-              <div className='icon-text-container'><FaHome className='icon-nav' size={'38px'} cursor='pointer'/><span>Home</span></div>
-            </li>
-            <li>
-
-              <div className='icon-text-container'><FaInfo className='icon-nav' size={'36px'} cursor='pointer'/><span>Historia</span></div>
-
-            </li>
-            <li>
-              <div className='icon-text-container'><BiNews className='icon-nav' size={'38px'} cursor='pointer'/><span>Noticias</span></div>
-            </li>
-            <li>
-              <div className='icon-text-container'><IoMdPhotos className='icon-nav' size={'38px'} cursor='pointer'/><span>Noticias</span></div>
-            </li>
+          <li>
+            <div className="icon-text-container" onClick={() => navigate("/")}>
+              <FaHome className="icon-nav" size={"38px"} cursor="pointer" />
+              <span>Home</span>
+            </div>
+          </li>
+          <li>
+            <div
+              className="icon-text-container"
+              onClick={() => navigate("/history")}
+            >
+              <FaInfo className="icon-nav" size={"36px"} cursor="pointer" />
+              <span>Historia</span>
+            </div>
+          </li>
+          <li>
+            <div
+              className="icon-text-container"
+              onClick={() => navigate("/news")}
+            >
+              <BiNews className="icon-nav" size={"38px"} cursor="pointer" />
+              <span>Noticias</span>
+            </div>
+          </li>
+          <li>
+            <div
+              className="icon-text-container"
+              onClick={() => navigate("/gallery")}
+            >
+              <IoMdPhotos className="icon-nav" size={"38px"} cursor="pointer" />
+              <span>Galeria</span>
+            </div>
+          </li>
         </ul>
       </div>
-      <div className='nav-user-container'>
+      <div className="nav-user-container" onClick={() => navigate("/register")}>
         <span>Iniciar Sesion</span>
-        <FaUserAlt className='icon-nav' size={'38px'} cursor='pointer' />
+        <FaUserAlt className="icon-nav" size={"38px"} cursor="pointer" />
       </div>
     </nav>
-  )
-}
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,18 +6,25 @@ import {
   } from "react-router-dom";
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { RegisterScreen } from '../components/auth/RegisterScreen';
+import { GalleryScreen } from '../components/mesa/GalleryScreen';
+import { InfoScreen } from '../components/mesa/InfoScreen';
 import { MesaScreen } from '../components/mesa/MesaScreen';
+import { NewScreen } from '../components/mesa/NewScreen';
 import { NavBar } from '../components/ui/NavBar';
 
 export const AppRouter = () => {
   return (
     <>
-    {/* <NavBar /> */}
-    <BrowserRouter>
+    <BrowserRouter> 
+        <NavBar />  
         <Routes>
             <Route path="/" element={<MesaScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/history" element={<InfoScreen />} />
+            <Route path="/news" element={<NewScreen />} />
+            <Route path="/gallery" element={<GalleryScreen />} />
+            
         </Routes>
     </BrowserRouter>
     </>
