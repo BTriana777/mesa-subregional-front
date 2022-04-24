3303e3c5f17718a930c61d36022e42b33337d744
Brandon Triana <trianab659@gmail.com>
2022-04-24 18:49:56 -0500

    i added mesa style to index style, also i added configuration for responsive mode in homescreen or mesascreen

--- src/components/mesa/MesaScreen.jsx
+++ src/components/mesa/MesaScreen.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export const MesaScreen = () => {
   return (
     <div className="screen-main home-main-container">
-      <img src="https://i.imgur.com/W67C9R4.jpg" alt="Dabeiba"   />
+      <div className="home-img-main" />
+      {/* <img src="https://i.imgur.com/W67C9R4.jpg" alt="Dabeiba"   /> */}
     </div>
   );
 };
